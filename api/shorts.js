72c685fc266e06214ae3ee52fdd6ffbdd6855ce9
BhagatPardeep <66918800+BhagatPardeep@users.noreply.github.com>
2026-01-15 12:41:00 +0530

    Update shorts.js

--- api/shorts.js
+++ api/shorts.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     if (!url) return res.status(400).json({ error: 'URL is required' });
 
     // --- PASTE YOUR RAPIDAPI KEY HERE ---
-    const rapidApiKey = "YOUR_RAPIDAPI_KEY_HERE"; 
+    const rapidApiKey = "ecd7d95a13msh21df9996905ca05p1ba68djsnc15adc9d6abe"; 
     // ------------------------------------
 
     try {
@@ -20,8 +20,8 @@ export default async function handler(req, res) {
         const response = await fetch(`https://yt-stream-download.p.rapidapi.com/GetShorts?url=${encodeURIComponent(url)}`, {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': ecd7d95a13msh21df9996905ca05p1ba68djsnc15adc9d6abe,
-                'X-RapidAPI-Host': 'yt-stream-download.p.rapidapi.com'
+                'X-RapidAPI-Key': rapidApiKey,
+                'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
             }
         });
 
@@ -60,3 +60,4 @@ function extractVideoID(url) {
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 }
+
